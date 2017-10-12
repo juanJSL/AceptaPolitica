b9ea395ba33c10777c015b887469d3eb33786565
Unknown <jjolula88@gmail.com>
2017-10-12 16:29:10 +0200

    Funciona
    Funciona pero no estan controladas las excepciones

--- app/src/main/java/com/jj/aceptapolitica/MainActivity.java
+++ app/src/main/java/com/jj/aceptapolitica/MainActivity.java
@@ -3,18 +3,53 @@ package com.jj.aceptapolitica;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final static int CODIGO = 0;
     Button btVerificar;
+    TextView resultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btVerificar = (Button) findViewById(R.id.verificaBT);
+        btVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzarVerifica();
+            }
+        });
     }
 
     public void lanzarVerifica(){
+        EditText etNombre = (EditText) findViewById(R.id.nombreET) ;
+        String nombre = etNombre.getText().toString();
+        EditText etEdad = (EditText) findViewById(R.id.edadET);
+        int edad = Integer.parseInt(etEdad.getText().toString());
+
         Intent i = new Intent(this, Verifica.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("NOMBRE", nombre);
+        bundle.putInt("EDAD", edad);
+        i.putExtras(bundle);
+        startActivityForResult(i, CODIGO);
     }
+
+    protected void onActivityResult( int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODIGO && resultCode == RESULT_OK) {
+            String respuesta= "Hola";
+            respuesta = data.getExtras().getString("RESULTADO");
+            resultado = (TextView) findViewById(R.id.resultadoTV);
+            resultado.setText(respuesta);
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/jj/aceptapolitica/Verifica.java
+++ app/src/main/java/com/jj/aceptapolitica/Verifica.java
@@ -1,7 +1,11 @@
 package com.jj.aceptapolitica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Verifica extends AppCompatActivity {
 
@@ -9,5 +13,47 @@ public class Verifica extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verifica);
+
+        //------Relaciono el textView y escribio el texto con los datos obtenidos del MainActivity
+        TextView nombreTV = (TextView) findViewById(R.id.nombreTV);
+        final Bundle bundle = getIntent().getExtras();
+        nombreTV.setText("Hola " + bundle.get("NOMBRE") + " ¿Aceptas lapolitica de privacidad?");
+        //int edad =
+
+
+
+
+        ///*-*------------------------------------------------------------------------------------
+        //------Relaciono los botonese indico que deben hacer cuando se pulsen
+            //------Boton aceptar
+        Button btAcepto = (Button) findViewById(R.id.aceptoBT);
+        btAcepto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String respuesta;
+                if(bundle.getInt("EDAD")>=18)
+                    respuesta = "Aceptada sin restricciones";
+                else
+                    respuesta = "Aceptada con restricciones";
+
+                Intent i = new Intent();
+                i.putExtra("RESULTADO", respuesta);
+                setResult(RESULT_OK, i);
+                finish();
+            }
+        });
+        //------Boton rechazar
+        Button btRechazo = (Button) findViewById(R.id.rechazoBT);
+        btRechazo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String respuesta = "Rechazada";
+                Intent i = new Intent();
+                i.putExtra("RESULTADO", respuesta);
+                setResult(RESULT_OK, i);
+                finish();
+            }
+        });
+       // -------------------------------------------------------------------------------------*/
     }
 }
